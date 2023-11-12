5e273740bf0ab774bb8415bf30c364a1b4ee75c1
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-11-12 09:23:51 +0100

    a04e: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A042F Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a04e.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a04e-user \
+    omni_a04e-userdebug \
+    omni_a04e-eng
